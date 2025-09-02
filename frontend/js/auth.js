df0e5b2bd4aa66aa5ecfbeeaa610aee66fedeaee
Mgalindo15 <Majogamon15@gmail.com>
2025-09-01 20:49:49 -0500

    auth functions

---
+++ frontend/js/auth.js
@@ -0,0 +1,33 @@
+import { Api } from "./api.js";
+
+export async function loginFlow(username, password) {
+    // set httpOnly cookie -->
+    const tok = await Api.login(username, password);
+    // if (tok?.access_token) localStorage.setItem("access_token", tok.access_token); 
+    //              --> Secondary validation via js header validation || not in use (uncomment api.js lines 11-13 && auth.js line 13, if using)
+    return await Api.me();
+}
+
+export async function logoutFlow() {
+    await Api.logout();
+    // localStorage.removeItem("access_token");
+}
+
+export async function requireAdminOrRedirect() {
+    try {
+        // pull logged in profile info
+        const me = await Api.me();
+        // not admin --> redirect
+        if (!me.is_admin) {
+            alert("Amin only.");
+            window.location.href = "/index.html";
+            return null;
+        }
+        // admin --> continue
+        return me;
+    } catch {
+        // error redirect (security)
+        window.location.href = "/index.html";
+        return null;
+    }
+}
